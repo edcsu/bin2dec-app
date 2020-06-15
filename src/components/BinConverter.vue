3efbfa7dfe05b979084b9d176a11f78c792f1af9
Keith Ssewanonda <keith@laboremus.no>
2020-06-16 00:56:01 +0300

    feat: add input form

--- src/components/BinConverter.vue
+++ src/components/BinConverter.vue
@@ -1,17 +1,74 @@
 <template>
   <v-container>
     <v-row class="text-center">
-      <v-col cols="12">
+      <v-col cols="6">
+            <v-form
+                ref="form"
+                v-model="valid"
+                lazy-validation
+            >
+                <v-text-field
+                v-model="number"
+                :counter="8"
+                :rules="numberRules"
+                label="Binary number"
+                required
+                ></v-text-field>
+
+                <v-btn
+                :disabled="!valid"
+                color="success"
+                class="mr-4"
+                @click="validate"
+                >
+                Validate
+                </v-btn>
+
+                <v-btn
+                color="error"
+                class="mr-4"
+                @click="reset"
+                >
+                Reset Form
+                </v-btn>
+
+                <v-btn
+                color="warning"
+                @click="resetValidation"
+                >
+                Reset Validation
+                </v-btn>
+            </v-form>
+
+
       </v-col>
     </v-row>
   </v-container>
 </template>
 
+
 <script>
   export default {
     name: 'BinConverter',
-
     data: () => ({
+      valid: true,
+      number: '',
+      numberRules: [
+        v => !!v || 'A binary number is required',
+        v => (v && v.length <= 8) || 'The binary number must be less than 8 characters',
+      ],
     }),
+
+    methods: {
+      validate () {
+        this.$refs.form.validate()
+      },
+      reset () {
+        this.$refs.form.reset()
+      },
+      resetValidation () {
+        this.$refs.form.resetValidation()
+      },
+    },
   }
 </script>
